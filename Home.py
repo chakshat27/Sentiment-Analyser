4d2c1f82a73d24cb8f258ff882cbecffbce6810a
Chakshat Bali <110087761+chakshat27@users.noreply.github.com>
2024-12-02 20:10:17 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,14 +5,14 @@ st.title("Sentiment Analyser App")
 
 st.subheader("What is Sentiment Analysis?")
 
-st.text("Sentiment analysis  is a natural language processing (NLP) technique that involves ")
+st.text_area("Sentiment analysis  is a natural language processing (NLP) technique that involves  the use of computational methods to determine the sentiment or emotional tone expressed in a piece of text. The goal of sentiment analysis is to understand and classify the sentiment of the text as positive, negative, neutral")
 st.text(" the use of computational methods to determine the sentiment or emotional tone")
 st.text("expressed in a piece of text. The goal of sentiment analysis is to understand ")
 st.text("and classify the sentiment of the text as positive, negative, neutral.")
 
 st.subheader("Objectives of Project")
 
-st.text("1. Reviews Preprocessing and Cleaning")
+st.text("1. Preprocessing and Cleaning")
 st.text("2. Story Generation and Visualization from reviews")
 st.text("3. Extracting Features from Cleaned reviews")
 st.text("4. Model Building: Sentiment Analysis")
