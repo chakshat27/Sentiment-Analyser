00b0bcc5792637407420bbfe0c6affff85298f8d
Chakshat Bali <110087761+chakshat27@users.noreply.github.com>
2024-12-09 20:33:30 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -2,17 +2,25 @@ import streamlit as st
 
 st.title("Sentiment Analyser App")
 
-
 st.subheader("What is Sentiment Analysis?")
-
 st.text("Sentiment analysis  is a natural language processing (NLP) technique that involves ")
 st.text(" the use of computational methods to determine the sentiment or emotional tone")
 st.text("expressed in a piece of text. The goal of sentiment analysis is to understand ")
 st.text("and classify the sentiment of the text as positive, negative, neutral.")
 
 st.subheader("Objectives of Project")
-
 st.text("1. Reviews Preprocessing and Cleaning")
 st.text("2. Story Generation and Visualization from reviews")
 st.text("3. Extracting Features from Cleaned reviews")
 st.text("4. Model Building: Sentiment Analysis")
+
+# Add a footer section
+st.markdown(
+    """
+    <hr>
+    <footer style="text-align: center; font-size: small; color: gray;">
+        © 2023 Sentiment Analyser App.| Created by Chakshat Bali , Savi Chopra
+    </footer>
+    """,
+    unsafe_allow_html=True
+)
